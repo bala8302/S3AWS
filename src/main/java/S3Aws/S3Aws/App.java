353abdce14c81d6d6d07e9bb67887eb81479b68b
bala8302 <55492151+bala8302@users.noreply.github.com>
2020-07-19 13:11:17 +0530

    s3awscommit

---
+++ src/main/java/S3Aws/S3Aws/App.java
@@ -0,0 +1,13 @@
+package S3Aws.S3Aws;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
